b9aeb02c67428f5554126e08807553b9ef0dba48
gautam <gautam@tripwizard.org>
2026-07-23 00:13:58 +0530

    Add Terraform configuration for Azure Resource Group

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "subscription_id" {
+  type    = string
+  default = "cd7a0e25-56d3-45b2-bc34-4ed7052adf80"
+}
+
+variable "location" {
+  type    = string
+  default = "Central India"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "rg-angular-demo"
+}
